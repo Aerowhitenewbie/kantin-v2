e337024e1c073b81065672e6c3f2c0b11e9aeeae
Aerowhitenewbie <dsemaya91@gmail.com>
2026-07-31 22:40:51 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // This file is served to every browser. Use only the Supabase publishable/anon key here.
 // Never put a `sb_secret_...` or service-role key in this file.
 window.SUPABASE_CONFIG = {
-  url: 'https://YOUR_PROJECT.supabase.co',
-  anonKey: 'YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY'
+  url: 'https://zblrioekvgcvgemfnita.supabase.co/rest/v1/',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibHJpb2VrdmdjdmdlbWZuaXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MDUyNTUsImV4cCI6MjEwMTA4MTI1NX0._qJ_s2vsgqzOfs54vWXM1W7PeEVVwF4EnUhNIKwFQis'
 };
